f2bd387ccd528affcbfd95bee885b32561f55fde
Tin2 <zybj@yahoo.com>
2017-10-30 16:48:23 +0000

    Add utt2spk

--- script/run.py
+++ script/run.py
@@ -5,11 +5,13 @@ path=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(path)
 from helper.audio import audio
 from helper.spk2gender import spk2gender
+from helper.utt2spk import utt2spk
 from helper.wav2scp import wav2scp
 
 if __name__ == '__main__':
-    name = "{0}/config/context.yaml".format(path)
+    name = "{0}/config/run.context".format(path)
     context = yaml.load(open(name))
     #audio(context).run()
     #spk2gender(context).run()
-    wav2scp(context).run()
+    #wav2scp(context).run()
+    utt2spk(context).run()
