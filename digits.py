68c77934ecc5f9cfa847b168453ef2ebcb14788c
Tin2 <zybj@yahoo.com>
2017-10-30 15:28:15 +0000

    Update digits.py

--- digits.py
+++ digits.py
@@ -10,11 +10,10 @@ class audio(object):
     def __init__(self, context):
         self.context = context
 
-    def map(self, sdir, tdir):
+    def map(self, sdir):
         """
         Map .m4a file names into .wav file names
         sdir: source directory containing .m4a files.
-        tdir: target directory containing .wav files.
         return: A list of .m4a, .wav names.
         """
         result = []
@@ -28,25 +27,25 @@ class audio(object):
                 else:
                    s = i
                    e = j - 1
-                tgt ='{0}/1_{1}_{2}.wav'.format(tdir, s, e)
+                tgt ='{0}/1_{1}_{2}.wav'.format(s, s, e)
                 src = glob.glob(pattern)
-                result.append((src[0], tgt))
+                result.append((s, src[0], tgt))
          # Do [2-9]
         for j in range(2, 10):
             pattern ='{0}/*({1})*'.format(sdir, j)
-            tgt ='{0}/1_0_{1}.wav'.format(tdir, j-1)
+            tgt ='0/1_0_{0}.wav'.format(j-1)
             src = glob.glob(pattern)
-            result.append((src[0], tgt))
+            result.append((0, src[0], tgt))
         # Do ffirst one.      
         pattern ='{0}/Recording.m4a'.format(sdir)
-        tgt ='{0}/1_0_0.wav'.format(tdir)
+        tgt ='0/1_0_0.wav'
         src = glob.glob(pattern)
-        result.append((src[0], tgt))
+        result.append((0, src[0], tgt))
         # Do last one.      
         pattern ='{0}/*(100)*'.format(sdir)
-        tgt ='{0}/1_9_9.wav'.format(tdir)
+        tgt ='9/1_9_9.wav'
         src = glob.glob(pattern)
-        result.append((src[0], tgt))
+        result.append((9, src[0], tgt))
         return result
 
     def m4a_to_wav(self, src, tgt):
@@ -62,23 +61,42 @@ class audio(object):
         rc = pipe.returncode
         return rc, out, err
 
-    def transform(self, src_tgt):
+    def transform(self):
         """
-        Transform m4a into wav in batch.
-        src_tgt: list of src and tgt names.
+        Split train and test data.
         """
-        for src, tgt in src_tgt:
-            rc, out, err = self.m4a_to_wav(src, tgt)
-            if not rc:
-                print("Failed for {0} {1}".format(src, tgt))
+        s_dir = '/work/data' 
+        t_root = '/work/kaldi/egs/digits/digits_audio' 
+        stn = self.map(s_dir)
+        #print(stn)
+        # Train data
+        data_type = 'train'
+        for e in range(9):
+            for speaker, src, tgtn in stn:
+                if speaker == e: 
+                    tgt = "{0}/{1}/{2}".format(t_root, data_type, tgtn)
+                    tgt_dir = os.path.dirname(tgt)
+                    if not os.path.exists(tgt_dir):
+                        os.makedirs(tgt_dir)
+                    #print(speaker, src, tgt)
+                    self.m4a_to_wav(src, tgt)
+        # Test data.
+        data_type = 'test'
+        for e in [9]:
+            for speaker, src, tgtn in stn:
+                if speaker == e: 
+                    tgt = "{0}/{1}/{2}".format(t_root, data_type, tgtn)
+                    tgt_dir = os.path.dirname(tgt)
+                    if not os.path.exists(tgt_dir):
+                        os.makedirs(tgt_dir)
+                    #print(speaker, src, tgt)
+                    self.m4a_to_wav(src, tgt)
+
 
     def run(self):
         """
         """
-        sdir = '/work/data' 
-        tdir = '/work/kaldi/egs/digits/digits_audio' 
-        src_tgt = self.map(sdir, tdir)
-        print(src_tgt)
+        self.transform()
  
 if __name__ == '__main__':
     name = "context.yaml"
